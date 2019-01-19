71cbd349b1d7fa1706255d6900b90090e8205529
Leslie <lesliejmanrique@gmail.com>
2019-01-18 19:53:34 -0500

    added code to refresh tokens

--- server/src/modules/mailer.js
+++ server/src/modules/mailer.js
@@ -1,9 +1,12 @@
+// npm install nodemailer googleapis
 const nodemailer = require('nodemailer');
+const {google} = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
 const fillTemplate = require('es6-dynamic-template');
 const fs = require('fs');
 
 module.exports = {
-    sendInterestRequest: (body) => {
+    sendInterestRequest: async (body) => {
         let {name, email, user_type, phone_number, organization} = body;
         if (user_type == 0){
             user_type = 'peer';
@@ -19,8 +22,23 @@ module.exports = {
 
         const fs_read = fs.readFileSync('src/static/interest_email.html', 'utf8');
         const content = fillTemplate(fs_read, {name, email, user_type, phone_number, organization});
+        
+        const oauth2Client = new OAuth2(
+            process.env.GOOGLE_CLIENT_ID,
+            process.env.GOOGLE_CLIENT_SECRET,
+            "https://developers.google.com/oauthplayground" // Redirect URL
+            );
+        
+        // get a new access token
+        oauth2Client.setCredentials({
+            refresh_token: process.env.GOOGLE_REFRESH_TOKEN
+        });
+        
+        const tokens = await oauth2Client.refreshAccessToken()
+        const accessToken = tokens.credentials.access_token
 
-        const tranporter = nodemailer.createTransport({
+        // const accessToken = ''
+        const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
                 type: 'oauth2',
@@ -28,6 +46,7 @@ module.exports = {
                 clientId: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
                 refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+                access_token: accessToken
             }
            
         });
@@ -39,17 +58,11 @@ module.exports = {
             html: content, 
             replyTo: process.env.EMAIL_USER_RECEIVER
         };
-       
 
-        tranporter.sendMail(mailOptions, function(err, res){
-            console.log("sending");
-            if(err){
-                console.log("There was an error in sending your email. Please Try again", err);
-            }
-            else{
-                console.log('Email sent!'
-                );
-            }
-        });
+        transporter.sendMail(mailOptions, (error, response) => {
+            error ? console.log(error) : console.log(response);
+            transporter.close();
+       });
+       
     }
 }
