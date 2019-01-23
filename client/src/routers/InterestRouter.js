22e59d6e9665e597440b46819dfab34f1d095702
Anna Chiu <awc316@nyu.edu>
2019-01-23 00:01:18 -0500

    Redirect to form instead of welcome page on / route

--- client/src/routers/InterestRouter.js
+++ client/src/routers/InterestRouter.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import InterestForm from "../components/InterestForm";
-import { WelcomePage, ThankYouPage } from "../components/InterestPages";
+import { ThankYouPage } from "../components/InterestPages";
 import "../styles/interest.css";
 
 const InterestRouter = () => (
 	<BrowserRouter>
 		<Switch>
-			<Route exact path="/" component={WelcomePage} />
-			<Route path="/interest_form" component={InterestForm} />
+			<Route exact path="/" component={InterestForm} />
 			<Route path="/thank_you" component={ThankYouPage} />
 		</Switch>
 	</BrowserRouter>
