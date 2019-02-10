a34a4d24ec6906b430f3fe3c78280afb532d98bb
Anna Chiu <awc316@nyu.edu>
2019-02-09 23:43:35 -0500

    Users table CRUD functions and router

--- server/src/routers/users.js
+++ server/src/routers/users.js
@@ -5,10 +5,12 @@ const usersController = require('../controllers').users;
 const users = [];
 let id = 1;
 
-
 router
   .get('/users',usersController.list)
-  .post('/users', usersController.create);
+  .post('/users', usersController.create)
+  .get('/find_user/:userId', usersController.retrieve)
+  .post('/update_user/:userId', usersController.update)
+  .delete('/delete_user/:userId', usersController.destroy);
 
 //testing router 
 // router
