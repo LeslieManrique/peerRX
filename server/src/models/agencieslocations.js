d62e3720f8b52d4360079d6f713ee35c1fcfbaf4
Anna Chiu <awc316@nyu.edu>
2019-02-16 14:42:14 -0500

    Prevent id field from automatically generating

--- server/src/models/agencieslocations.js
+++ server/src/models/agencieslocations.js
@@ -13,5 +13,9 @@ module.exports = (sequelize, DataTypes) => {
   AgenciesLocations.associate = function(models) {
     // associations can be defined here
   };
+
+  // remove the id column that is added in by sequelize when there is no primary key
+  AgenciesLocations.removeAttribute('id');
+  
   return AgenciesLocations;
 };
